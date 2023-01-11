c2648264daae171939f3fcefdc049a3d77e3fac7
Korlan <kora.argynova@gmail.com>
2023-01-11 10:59:04 +0300

    Added final script, scenes and obsres

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -4,23 +4,24 @@ dir = r"C:\Users\Public\Documents\Integra\Lumicept 11.31 x64\scenes\lab2"
 scene_file = dir + "\initial.iof"
 scene_output_file = dir + "\cornel_box0_sphere.iof"
 
-def main():
-	# Load scene
-	scene = OpenScene(scene_file)
-	notebook = scene.Notebook()
+def set_imap_params(imap):
+	imap.SetObserverAsAccSource(obsNode, calc_imap=False)
+	imap.req_acc = 0.01
+	imap.time_limit = 3600
 
-	camera = Camera(view_angle=55, targ_dist=3300)
-	camera.targ_dist = 3300
-	# set origin
-	# set target
-	camera.Translate(-910.77, -2915.73, 1248.6)
-	
-	camera.Apply()
-	#notebook.AddCamera(camera)
+def run_mcrt(kernel, scene):
+	kernel.LoadScene(scene)
+	print("run MCRT simulation...")
+	kernel.CalculateIMaps()
 
-	obs = PlaneObserver(name="plane_2D_luminance")
+def modify_luminance(scene, node_name: str, lum_value: float):
+	node = scene.GetNode(node_name)
+	node.shape.parts[0].surf_attrs.self_lum = lum_value
+
+def get_plane_observer(name="plane_2D_luminance", angle=1.0):
+	obs = PlaneObserver(name=name)
 	obs.res = (640, 480)
-	obs.thresh_ang = 1
+	obs.thresh_ang = angle
 	obs.phenom = LUM
 	obs.ortho = False
 	obs.focal_dist = 3300
@@ -30,13 +31,72 @@ def main():
 	obs.glob_attached = True
 	obs.occlusion = True
 
-	obsNode = ObserverNode(obs, name="plane_2D_luminance")
+	return obs
+
+def get_lens_observer(name="lens_2D_luminance"):
+	obs = LensObserver(name=name)
+	obs.res = (640, 480)
+	obs.view_angle = 42.65386
+	obs.pupil_diam = 50
+	obs.focal_length = 100
+	obs.focusing_dist = 3300
+	obs.image_dist = 103.125
+
+	return obs
+
+def main():
+	scene = OpenScene(scene_file)
+	notebook = scene.Notebook()
+
+	""" Add and set camera """
+	camera = Camera(view_angle=55, targ_dist=3300)
+	camera.Translate(-910.77, -2915.73, 1248.6)
+	camera.Rotate(67.768, 0, -17.347)
+	### set the target coordinates ???
+	### "keep target point" flag ???
+	### set the twist angle ???
+	camera.Apply()
+	scene.camera = camera
+	#notebook.AddCamera(camera)
 	
+	""" Add and set the Plane Observers for angles 
+		(deg): 1, 10, 30, 60, 90 """
+
+	for angle in [1, 10, 30, 60, 90]:
+		name = "plane_2D_luminance_{}deg".format(angle)
+		plane_obs = get_plane_observer(name=name, angle=angle)
+		obsNode = ObserverNode(plane_obs, name=name)
+		obsNode.tr.pos = (0, 0, 0)
+		obsNode.Rotate(67.768, 0, -17.347)
+		obsNode.tr.scale = (3435.743, 2576.807, 1)
+		
+		scene.AddNode(obsNode)
+
+	""" Add and set the Lens Observer """
+	lens_obs = get_lens_observer()
+	obsNode = ObserverNode(lens_obs, name="lens_2D_luminance")
+	obsNode.tr.pos = (-910.77, -2915.73, 1248.6)
+	obsNode.Rotate(67.768, 0, -17.347)
+	
+	node_names = ["floor shape node", "100nit", "75nit", "50nit"]
+	lum_values = [35, 55, 125, 350]
+	
+	for i in range(4):
+		modify_luminance(scene, node_names[i], lum_values[i])
+
 	scene.AddNode(obsNode)
-	#scene_output_file = dir + '\Test.iof'
-	#scene.Save(scene_output_file)
+
 	LoadScene(scene)
 
+	#scene_output_file = dir + '\intial_set_observers.iof'
+	#scene.Save(scene_output_file)
+
+	imap = scene.IMapsParams()
+	set_imap_params(imap)
+	
+	kernel = Kernel()
+	run_mcrt(kernel, imap)
+	
 
 if __name__ == "__main__":
 	main()
